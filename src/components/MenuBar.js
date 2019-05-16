80ce8f23e2c18983b6cfbac9baa7d2def35a00af
Sofia <jonssonsofia93@gmail.com>
2019-05-16 10:27:27 -0700

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -4,7 +4,7 @@ const MenuBar = (props) => {
 
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -14,20 +14,26 @@ const MenuBar = (props) => {
   */
 
   return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
+
+    <div  className="ui four item menu">
+
+      <a onClick={ () => props.onClick("profile")}
+      className ={"item" + (props.activeStatus === "profile" ? "active": null )}  id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={ () => props.onClick("photo")}
+      className ={"item" + (props.activeStatus === "photo" ? "active": null )} id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={ () => props.onClick("cocktail")}
+      className ={"item" + (props.activeStatus === "cocktail" ? "active": null )} id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
+      <a onClick={ () => props.onClick("pokemon")}
+       className ={"item" + (props.activeStatus === "pokemon" ? "active": null )} id="pokemon">
         <i className=" themeisle large icon" id="pokemon"/>
       </a>
     </div>
